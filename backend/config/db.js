d9b2587017d35cb305e842c4a833761b8f4d4df7
Giri velan <144417165+giri1043@users.noreply.github.com>
2025-10-14 15:46:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri = "mongodb+srv://ucs-modulars:<db_password>@cluster0.zdlx2wk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://ucs-modulars:ucs_db_user@cluster0.zdlx2wk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
   try {
@@ -11,7 +11,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log(`MongoDB connected successfully: ${conn.connection.host}`); 
+    console.log(`MongoDB connected successfully: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
